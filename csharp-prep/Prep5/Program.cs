eb3aa8ca053631216ca3ce6ec7244de9d386fdd7
Kallen Joos <kallen.joos@gmail.com>
2026-01-15 10:37:44 -0700

    I made this program interact with the user

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,58 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("What is your name? ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+     static int PromptUserNumber()
+    {
+        Console.Write("What is your favorite number? ");
+        int favNum = int.Parse(Console.ReadLine());
+        return favNum;
+    }
+
+    static void PromptUserBirthYear(out int birthYear)
+    {
+        Console.Write("What is your birth year? ");
+        birthYear = int.Parse(Console.ReadLine());
+    }
+
+    static int SquareNumber(int userNum)
+    {
+        int squared = userNum * UserNum;
+        return squared;
+    }
+
+    static void DisplayResult(string userName, int squaredNum, int birthYear)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {squaredNum}");
+        int userAge = 2026 - birthYear;
+        Console.WriteLine($"{userName}, you will turn {userAge} this year.");
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        int birthYear;
+
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNum = PromptUserNumber();
+        PromptUserBirthYear(out birthYear);
+
+        int squaredNum = SquareNumber(userNum);
+
+        DisplayResult(userName, squaredNum, birthYear);
     }
 }
