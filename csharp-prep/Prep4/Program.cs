f486c209775ce55a8dc9c62acb62acdb24aeb40f
Kallen Joos <kallen.joos@gmail.com>
2026-01-14 17:55:54 -0700

    Made a program that adds up numbers and finds the average

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Diagnostics;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int userNum;
+        int totalSum = 0;
+        int highestNum = 0;
+
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        do
+        {
+            Console.Write("Enter Number: ");
+            string userInput = Console.ReadLine();
+
+            userNum = int.Parse(userInput);
+            numbers.Add(userNum);
+        }
+        while(userNum != 0);
+            
+        for (int i = 0; i < numbers.Count; ++i)
+        {
+            totalSum += numbers[i];
+
+            if(numbers[i] > highestNum)
+            {
+                highestNum = numbers[i];
+            }
+        }
+
+        Console.WriteLine($"The total sum is: {totalSum}");
+        Console.WriteLine($"The average is: {totalSum /(numbers.Count - 1)}");
+        Console.WriteLine($"The largest number is: {highestNum}");
     }
 }
