9c0b52410ecafce670d6a020e3e71b06f5a8848a
Kallen Joos <kallen.joos@gmail.com>
2026-03-02 21:33:04 -0700

    I did the activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,23 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        Square square = new Square("greeen", 4);
+        shapes.Add(square);
+        Rectangle rectangle = new Rectangle("purple", 4, 3);
+        shapes.Add(rectangle);
+        Circle circle = new Circle("blue", 4);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}");
+            Console.WriteLine($"Area: {shape.GetArea()}");
+        }
+       
     }
 }
